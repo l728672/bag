42eace1d181d80a11b3f59719452dafc9e5364c6
l728672 <54706231+l728672@users.noreply.github.com>
2021-04-13 20:56:48 +0800

    Update py02.py

--- py02.py
+++ py02.py
@@ -4,7 +4,7 @@ import random
 import time
 with open('idkp1-10.txt') as f:
     allstr = f.readlines()
-
+#print(allstr)
 #读取组内数据
 def func(s):
     for i in range(len(allstr)):
@@ -13,15 +13,19 @@ def func(s):
             n=v1.split('=')[-1]
             n=eval(n)
             c=v2.split()[-1][:-1]
-
+#             print('diemnsion: ',n)
+#             print('cubage: ',c)
             p = allstr[i+3].split(',')
             v=p[:-1]
-
+            #print("物品的价值为：",p)
             w = allstr[i+5].split(',')
             w=w[:-1]
-
+            #print("物品的重量为：",w)
+#             print('\nprofit\t','weight')
+#             for n,m in zip(p,w):
+#                 print(n,'\t',m)
+#             break
             return n,c,v,w
-
 #绘制重量——价值的散点图
 def draw_scatter(n,v,w):
     x=[]
@@ -38,7 +42,6 @@ def draw_scatter(n,v,w):
     plt.xlabel("W")
     plt.ylabel("V")
     plt.show()
-
 #对价值与重量的比值进行非递增排序
 def datasorted(w,v):
     v_w=[]
@@ -50,30 +53,34 @@ def datasorted(w,v):
     v_w.sort(reverse=True)
     print(v_w)
 def pack(w,v,C):
+    """
+    w:list[list,list,...]
+    v:list[list,list,...]
+    """
     begin=time.time()
-
     n_list = [3, 3, 3, 3, 3, 3, 3, 3, 3, 3]
-
     dp = [0 for _ in range(C + 1)]
     for i in range(1, len(w) + 1):  # 物品循环
         for j in reversed(range(1, C + 1)):  # 剩余体积循环
-            for k in range(n_list[i-1]):  # 别的和0，1背包一样 就是这里枚举一下每个组内的值，在每个组内选出一个max值
-                if j-w[i-1][k] >= 0:
-                    dp[j] = max(dp[j], dp[j- w[i-1][k]] + v[i-1][k])
-    print("最大价值：",dp[C])
+            for k in range(n_list[i - 1]):  # 别的和0，1背包一样 就是这里枚举一下每个组内的值，在每个组内选出一个max值
+                if j - w[i - 1][k] >= 0:
+                    dp[j] = max(dp[j], dp[j - w[i - 1][k]] + v[i - 1][k])
+    #print("最大的价值为：",dp[C])
     end=time.time()
-    print("运行时间为：",end-begin)
+    t=end-begin
+    #print("运行时间为：",end-begin)
+    return dp[C],t
 
 #n,c,v,w=func('IDKP0')
+#n=eval(n)
 
 if __name__=='__main__':
     print("请输入你需要哪一组数据的文件：")
     file=input()
     n,c,v,w=func(file)
-    print(n,c,v,w)
+    #print(n,c,v,w)
     draw_scatter(n,v,w)
     datasorted(w,v)
-
     v1=[int(x)for x in v]
     w1=[int(y)for y in w]
     v2=[v1[i:i+3] for i in range(0, len(v1),3)]   #3个3个分为一组
@@ -83,7 +90,10 @@ if __name__=='__main__':
     #print(v2,w2)
     #print(v1[1][1],type(v1[1][1]))
     C=int(c)
-    pack(w2,v2,C)
-
-
-
+    maxvalue,t=pack(w2,v2,C)
+    #print(t)
+    with open('结果.txt','w',encoding="utf-8") as f:
+        f.write("最大的价值为：")
+        f.write(str(maxvalue))
+        f.write("\n运行的时间为：")
+        f.write(str(t)+"S")
